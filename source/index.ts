64178a8ee1a1f16459d1deadfba5f059338092ea
Iurii <yurkimus@gmail.com>
2023-07-29 01:31:31 +0200

    reimplement with ramda

--- source/index.ts
+++ source/index.ts
@@ -1,98 +1,91 @@
-export type PipeParameters = [
-  init: Omit<RequestInit, 'headers'>,
-  headers: RequestInit['headers']
+import {
+  T,
+  __,
+  always,
+  any,
+  cond,
+  defaultTo,
+  head,
+  includes,
+  invoker,
+  juxt,
+  pathEq,
+  pipe,
+  prop,
+  split,
+  unless,
+} from 'ramda'
+
+const text = [
+  'text/plain',
+  'text/html',
+  'text/css',
+  'application/xml',
+  'application/x-www-form-urlencoded',
+  'application/xml-dtd',
+  'application/atom+xml',
+  'application/rss+xml',
+  'application/x-javascript',
 ]
 
-export type Pipe = (...parameters: PipeParameters) => Promise<PipeParameters>
+const json = [
+  'application/json',
+  'application/ld+json',
+  'application/json-patch+json',
+  'application/vnd.api+json',
+  'application/csp-report',
+]
 
-const mime = (response: Response) =>
-  response.headers.get('content-type')?.split(';')[0]
+const media = [
+  'audio/mpeg',
+  'audio/ogg',
+  'audio/wav',
+  'audio/webm',
+  'video/mp4',
+  'video/ogg',
+  'video/webm',
+  'image/jpeg',
+  'image/png',
+  'image/gif',
+]
 
-const text = (response: Response) => {
-  const clone = response.clone()
-
-  return response.text().then(value => [clone, value] as const)
+const raise = (error: any) => {
+  throw error
 }
 
-const json = (response: Response) => {
-  const clone = response.clone()
+const mime = pipe<[Response], Headers, string, string, string[], string>(
+  prop('headers'),
+  invoker(1, 'get')('Content-Type'),
+  defaultTo(''),
+  split(';'),
+  head
+)
 
-  return response.json().then(value => [clone, value] as const)
-}
+const isMime = (types: string[]) =>
+  pipe<[Response], string, (args_0: string) => boolean, boolean>(
+    mime,
+    includes,
+    any(__, types)
+  )
 
-const empty = (response: Response) =>
-  Promise.resolve([response, undefined] as const)
+const clone = (as: (...args: any) => any) => juxt([invoker(0, 'clone'), as])
 
-const parse = (response: Response) => {
-  switch (mime(response)) {
-    case 'text/html':
-    case 'text/plain':
-      return text(response)
+export const takeResponse = <Result extends any>(result: [Response, Result]) =>
+  result[0]
 
-    case 'application/json':
-      return json(response)
+export const takeParsed = <Result extends any>(result: [Response, Result]) =>
+  result[1]
 
-    default:
-      return empty(response)
-  }
-}
-
-const decide = (parameters: readonly [Response, any]) => {
-  const [response] = parameters
-
-  if (response.ok) {
-    return parameters
-  } else {
-    throw parameters
-  }
-}
-
-/**
- * @description Provides a litle bit cleaner syntax when unpucking promise result from the `request`
- */
-export const unwrap = <T>(parameters: readonly [Response, T]) => parameters[1]
-
-/**
- * @description Foundation of making requests
- */
-export const request = <T>(
-  info: URL,
-  init?: RequestInit
-): Promise<readonly [Response, T]> => fetch(info, init).then(parse).then(decide)
-
-export function pipe<First>(
-  first: () => Promise<First>
-): (initial: Promise<any>) => Promise<First>
-
-export function pipe<First, Second>(
-  first: () => Promise<First>,
-  second: () => Promise<Second>
-): (initial: Promise<any>) => Promise<First & Second>
-
-export function pipe<First, Second, Third>(
-  first: () => Promise<First>,
-  second: () => Promise<Second>,
-  third: () => Promise<Third>
-): (initial: Promise<any>) => Promise<First & Second & Third>
-
-export function pipe<First, Second, Third, Fourth>(
-  first: () => Promise<First>,
-  second: () => Promise<Second>,
-  third: () => Promise<Third>,
-  fourth: () => Promise<Fourth>
-): (initial: Promise<any>) => Promise<First & Second & Third & Fourth>
-
-/**
- * @description Function that provides a way to pipe request requests responses down to one promise
- */
-export function pipe(...pipes: (() => Promise<any>)[]) {
-  const chain = (previous: Promise<any>, next: () => Promise<any>) =>
-    previous.then(composed =>
-      next().then(response => ({
-        ...composed,
-        ...response,
-      }))
+export const request = <Result extends unknown>(
+  ...args: Parameters<typeof fetch>
+): Promise<[Response, Result]> =>
+  fetch(...args)
+    .then(
+      cond([
+        [isMime(text), clone(invoker(0, 'text'))],
+        [isMime(json), clone(invoker(0, 'json'))],
+        [isMime(media), clone(invoker(0, 'blob'))],
+        [T, clone(always(null))],
+      ])
     )
-
-  return (initial: Promise<object>) => pipes.reduce(chain, initial) as any
-}
+    .then(unless(pathEq(true, ['0', 'ok']), raise))
